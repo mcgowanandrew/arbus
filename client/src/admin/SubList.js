8322d42e22c305e4fc85982930e2a1e4d4ebd2a5
“helloandrewpaul” <mcgowanandrew@gmail.com>
2021-07-02 07:06:23 -0400

    submission list completed styled

--- client/src/admin/SubList.js
+++ client/src/admin/SubList.js
@@ -30,28 +30,95 @@ const SubList = ({ sub }) => {
   }
 
   return (
-    <Wrap>
-      Title: {sub.title}
-      <Button onClick={handleViewClick}>View</Button>
+    // <Wrap>
+    //   Title: {sub.title}
+    //   <Button onClick={handleViewClick}>View</Button>
+    //   <Button type="submit" onClick={deleteSubHandler}>Reject</Button>
+    // </Wrap>
+    <BookWrap key={sub._id}>
+    <BookCover src={sub.images} alt={sub.title} />
+    <Hover>
+      <TitleWrap>
+        <Title>{sub.title}</Title>
+        <Tog>{sub.photographer}</Tog>
+      </TitleWrap>
+     
+          <ButWrap>
+          <Button onClick={handleViewClick}>View</Button>
       <Button type="submit" onClick={deleteSubHandler}>Reject</Button>
-    </Wrap>
+          </ButWrap>
+   
+    </Hover>
+  </BookWrap>
   );
 };
-const Wrap = styled.div`
-  margin: 25px;
-  width: 300px;
+const ButWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-content: flex-end;
 `;
+
 const Button = styled.button`
   border: 2px solid #000;
-  padding: 5px;
+  padding: 5px 10px;
   background-color: #000;
   color: #fff;
-  margin-left: 10px;
+  font-weight: bold;
   &:hover {
     background-color: #fff;
     color: #000;
     cursor: pointer;
     transition: all 0.3s ease-in-out;
   }
+`;
+
+const TitleWrap = styled.div`
+width:100%;
+height:auto;
+`;
+
+const Hover = styled.div`
+  position: absolute;
+  display: flex;
+  padding: 15px;
+  flex-direction: column;
+  justify-content: space-between;
+  background: rgba(255, 255, 255, 0.9);
+  width: 300px;
+  height: 100%;
+  z-index: 10;
+  opacity: 0;
+  &:hover {
+    transition: all 0.5s ease-in-out;
+    opacity: 1;
+  }
+`;
+
+const Tog = styled.div`
+  width: 100%;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  font-weight: bold;
+  margin-bottom: 15px;;
+`;
+
+const BookWrap = styled.div`
+  width: 300px;
+  height: auto;
+  margin: 15px;
+  position: relative;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const BookCover = styled.img`
+  width: 300px;
+  height: auto;
+  margin: 0 auto;
 `;
 export default SubList;
